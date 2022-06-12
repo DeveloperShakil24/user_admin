8bc9e96d2c61ac7d8611f50c9c01edd04f7e2dbb
DeveloperShakil24 <developershakil24@gmail.com>
2022-06-12 13:11:59 +0600

    12/06/22

---
+++ Portfolio/admin/includes/navigation.php
@@ -0,0 +1,38 @@
+<!-- Main navigation -->
+<div class="sidebar-category sidebar-category-visible">
+    <div class="category-content no-padding">
+        <ul class="navigation navigation-main navigation-accordion">
+
+            <!-- Main -->
+            <li class="active"><a href="index.php"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
+            <li><a href="banner.php"><i class="icon-images3"></i> <span>Benners</span></a></li>
+            <li><a href="#"><i class="icon-gradient"></i> <span>Services</span></a></li>
+            <li><a href="#"><i class="icon-images3"></i> <span>Sections</span></a></li>
+            <li><a href="#"><i class="icon-images3"></i> <span>Our Project</span></a></li>
+            <li><a href="#"><i class="icon-images3"></i> <span>Our Staff</span></a></li>
+            <li><a href="#"><i class="icon-images3"></i> <span>Our Client</span></a></li>
+            <li><a href="#"><i class="icon-images3"></i> <span>Contact</span></a></li>
+            <li><a href="#"><i class="icon-images3"></i> <span>Contact Massage</span></a></li>
+            <li>
+                <a href="#"><i class="icon-gear"></i> <span>Back Office Setup</span></a>
+                <ul>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Designation</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#"><i class="icon-stack"></i> <span>Starter kit</span></a>
+                <ul>
+                    <li>
+                        <a href="#">3 columns</a>
+                        <ul>
+                            <li><a href="starters/3_col_dual.html">Dual sidebars</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <!-- /main -->
+        </ul>
+    </div>
+</div>
+<!-- /Main navigation -->
